aa81dc1d4f1a96f5d66051a66e9277df8ce1bc48
sabiha-bugendaitech <sabiha.anjum@bugendaitech.com>
2022-08-22 17:26:04 +0530

    first commit

--- src/screens/about/about.jsx
+++ src/screens/about/about.jsx
@@ -11,11 +11,8 @@ const About = () => {
         <Back name='About Us' title='About Us - Who We Are?' cover={img} />
         <div className='container flex mtop'>
           <div className='left row'>
-            <Heading title='Our Agency Story' subtitle='Check out our company story and work process' />
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
-            <button className='btn2'>More About Us</button>
+                      <button className='btn2'>More About Us</button>
           </div>
           <div className='right row'>
             <img src='./immio.jpg' alt='' />

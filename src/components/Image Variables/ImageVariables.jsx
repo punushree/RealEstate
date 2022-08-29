cab815611ec2b9beb5b39d1eb6d59f956e98dae8
RaviBugendaitech <ravi.singh@bugendaitech.com>
2022-08-29 13:42:21 +0530

    improved the file structure for text & images, added contents in footer section

---
+++ src/components/Image Variables/ImageVariables.jsx
@@ -0,0 +1,8 @@
+const Image = {
+  SliderImg1: require("../../assets/images/home2.jpg"),
+  SliderImg2: require("../../assets/images/home3.jpg"),
+  SliderImg3: require("../../assets/images/vill.jpg"),
+  SliderImg4: require("../../assets/images/villa.jpg"),
+  HomeLogo: require("../../assets/images/homelogo.png"),
+};
+export { Image };
---
+++ src/core/i18n/en.jsx
@@ -0,0 +1,62 @@
+export const en = {
+  label: {
+    exploreByLocations: "Explore by Locations",
+    findHomeInAmazingLocalities: "Find home in amazing localities.",
+    rentApartment: "Rent Apartment",
+    searchYourDreamHome: "Search Your Dream Home",
+    findBeautifulApartmentFlatAndVillas:
+      "Find beautiful apartment, flat and villas.",
+    beautifulInterio: "Beautiful interio",
+    findFullyFurnishedUnfurnishedApartment:
+      "Find fully furnished / unfurnished apartment",
+    previous: "Previous",
+    next: "Next",
+    ourHappyCustomers: "Our Happy Customers",
+    ronneGalle: "Ronne Galle",
+    villaOwner: "Villa Owner",
+    cardPara1:
+      " “ We have found our dream home here. Amazing options to consider” ",
+    missyLimana: "Missy Limana",
+    cardPara2:
+      " “ Thank you so much for all your assistance in finding us a very decent expat tenant for our villa @Abode. We are very much satisfied with the service of Abode®. ” ",
+    peterMartha: "Peter Martha",
+    tenant: "Tenant",
+    cardPara3:
+      " “They are a very professional real estate agency in Chennai with good selection of places. More importantly, they spend time with the client to show variety of options. ” ",
+    hannaLisem: "Hanna Lisem",
+    cardPara4:
+      "“ I am a proud owner of a beautiful Villa.Have already referred you to a couple of friends! Thanks and Regards ”",
+    aboutUs: "ABOUT US",
+    aboutUsText1:
+      "We at abode strive to get customer happiness and satisfaction",
+    forMoreDetailContactUs: "For more detail contact us",
+    contactNumber: "+4343478734",
+    viewDetail: "View Detail",
+    selectLocation: "Select Location",
+    selectCategory: "Select Category",
+    selectPrice: "Select Price",
+    selectArea: "Select Area",
+    bedrooms: "Bedrooms:",
+    bathroom: "Bathroom:",
+    apartment: "Apartment",
+    villa: "Villa",
+    residentialBuilding: "Residential Building",
+    contactName: "contact name",
+    price: "price:",
+    type: "Type",
+    contact: "Contact",
+    villas: "Villas",
+  },
+
+  headerNames: {
+    home: "Home",
+    about: "About",
+    buyProperty: "Buy Property",
+    rentApartment: "Rent Apartment",
+    findRentedApartmentAtAffordableRates:
+      "Find rented apartment at affordable rates.",
+  },
+  footerNames: {
+    footerMainText: "© 2023 Abode - Designed by Bugendaitech.",
+  },
+};
